85d9106ccee4169f0c1043037a9863e8e9ca453a
LightAi39 <66924896+LightAi39@users.noreply.github.com>
2024-05-30 17:22:39 +0200

    added main server auth check

--- UserService/Logic/LoginLogic.cs
+++ UserService/Logic/LoginLogic.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using Contracts;
 using Microsoft.EntityFrameworkCore;
 using UserService.DTOs;
 using UserService.Interfaces;
@@ -9,27 +11,66 @@ public class LoginLogic
 {
     private readonly AppDbContext _dbContext;
     private readonly IJwtProvider _jwtProvider;
+    private readonly IHttpClientFactory _httpClientFactory;
 
-    public LoginLogic(AppDbContext dbContext, IJwtProvider jwtProvider)
+    public LoginLogic(AppDbContext dbContext, IJwtProvider jwtProvider, IHttpClientFactory httpClientFactory)
     {
         _dbContext = dbContext;
         _jwtProvider = jwtProvider;
+        _httpClientFactory = httpClientFactory;
     }
 
     public async Task<LogicResponse<string>> HandleLoginAsync(LoginDto loginDto)
     {
-        User? user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == loginDto.Id);
+        string id;
+        try
+        {
+            id = await GetMainServerAuthorizationAndId(loginDto.AuthToken);
+        }
+        catch (Exception e)
+        {
+            if (e.Message == "Invalid authorization token.")
+            {
+                return LogicResponse<string>.Fail("Invalid credentials", LogicResponseType.Unauthorized);
+            }
+
+            throw;
+        }
+        
+        if (id != loginDto.Id)
+        {
+            return LogicResponse<string>.Fail("Invalid credentials combination", LogicResponseType.Unauthorized);
+        }
+        
+        User? user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Id == id);
 
         if (user is null)
         {
             return LogicResponse<string>.Fail("User not found", LogicResponseType.NotFound);
         }
         
-        // TODO: check auth with main server
-        
-        
         string token = _jwtProvider.GenerateJwtToken(user);
         
         return LogicResponse<string>.Ok(token);
     }
+
+    private async Task<string> GetMainServerAuthorizationAndId(string authToken)
+    {
+        var client = _httpClientFactory.CreateClient();
+        client.DefaultRequestHeaders.Add("Cookie", authToken);
+        
+        var response =
+            await client.GetAsync($"{Constants.blMainServerUrl}user/id");
+        response.EnsureSuccessStatusCode();
+
+        if (response.StatusCode == HttpStatusCode.NoContent)
+        {
+            throw new Exception("Invalid authorization token.");
+        }
+
+        var responseData = await response.Content.ReadAsStringAsync();
+
+        return responseData;
+    }
+    
 }
